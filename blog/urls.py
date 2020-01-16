230e4c890323244f593dc7fbbc4f48880a16b8de
celso <celsobadu@hotmail.com>
2020-01-16 15:27:17 -0300

    celso

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,13 @@ urlpatterns = [
 	path('', views.post_list, name='post_list'),
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 	path('post/new', views.post_new, name='post_new'),
+	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
+
+'''comando para subir pro servidor local pro github: usar o shell local do pc
+git add --all
+git commit -m "#"
+git push
+
+comando para puxar do github para um servidor de internet: usar o shell do pythonanywhere
+git pull'''
